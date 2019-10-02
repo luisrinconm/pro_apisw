4d5f09f6fc771de43c12272f6f5e19232eecc773
jpsantamaria <51415602+jpsantamaria@users.noreply.github.com>
2019-10-01 23:27:43 -0500

    Actualización del home y creación de personas

---
+++ personas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="include/bootstrap/css/bootstrap.min.css">
+    <script src="include/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Start War</title>
+
+</head>
+<body>
+    <div class="container-fluid">
+        <?php
+            require_once('html/head.html');
+
+            require_once('section/personas.html');
+
+            require_once('html/footer.html');
+        ?>
+    </div>
+</body>
+</html>
---
+++ section/home.php
@@ -0,0 +1,44 @@
+<section>
+    <div class="row">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+            <?php require_once('carousel.html') ?>
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+</section>
+<section>
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h2>Lista de Películas</h2>
+        </div>
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-3">
+                    <figure class="figure">
+                        <img src="img/peliculas/pelicula1.jpg" alt="" class="img-thumbnail">
+                        <figcaption class="figure-caption text-right">La Amenaza Fantasma</figcaption>
+                    </figure>
+                </div>
+                <div class="col-lg-3">
+                    <figure class="figure">
+                        <img src="img/peliculas/pelicula2.jpg" alt="" class="img-thumbnail">
+                        <figcaption class="figure-caption text-right">La Amenaza Fantasma</figcaption>
+                    </figure>
+                </div>
+                <div class="col-lg-3">
+                    <figure class="figure">
+                        <img src="img/peliculas/pelicula3.jpg" alt="" class="img-thumbnail">
+                        <figcaption class="figure-caption text-right">La Amenaza Fantasma</figcaption>
+                    </figure>
+                </div>
+                <div class="col-lg-3">
+                    <figure class="figure">
+                        <img src="img/peliculas/pelicula4.jpg" alt="" class="img-thumbnail">
+                        <figcaption class="figure-caption text-right">La Amenaza Fantasma</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
